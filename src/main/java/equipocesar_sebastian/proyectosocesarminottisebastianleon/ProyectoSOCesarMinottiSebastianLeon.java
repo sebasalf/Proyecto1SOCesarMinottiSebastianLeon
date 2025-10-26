eda5620aa66063ae1998bf574b7c51a35154d7ff
sebasalf <122901813+sebasalf@users.noreply.github.com>
2025-10-26 03:13:28 -0400

    Primer comit con la interfaz grafrica

---
+++ src/main/java/equipocesar_sebastian/proyectosocesarminottisebastianleon/ProyectoSOCesarMinottiSebastianLeon.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package equipocesar_sebastian.proyectosocesarminottisebastianleon;
+
+/**
+ *
+ * @author sebas
+ */
+public class ProyectoSOCesarMinottiSebastianLeon {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> new JFrame_Principal().setVisible(true));
+        
+        
+        
+      
+    }
+}
